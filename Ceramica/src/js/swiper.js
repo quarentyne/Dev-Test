c8ca72bbd8015995e15aebf441679d9af1432618
quarentyne <peresada1997@gmail.com>
2022-08-06 19:35:58 +0300

    section projects isn't work :(

--- Ceramica/src/js/swiper.js
+++ Ceramica/src/js/swiper.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js';
 
-const swiper = new Swiper('.underheader-slider', {
+const uderheaderSwiper = new Swiper('.underheader-slider', {
   slidesPerView: 1,
   loop: true,
   pagination: {
@@ -13,3 +13,27 @@ const swiper = new Swiper('.underheader-slider', {
     prevEl: '.underheader-nav__prev',
   },
 });
+
+// const projectsSwiper = new Swiper('.full-slider', {
+//   // slidesPerView: 0.8,
+//   loop: true,
+//   navigation: {
+//     nextEl: '.projects__next',
+//     prevEl: '.projects__prev',
+//   },
+// });
+
+// const swiper1 = new Swiper(".mini-carousel", {
+//   loop: true,
+//   spaceBetween: 10,
+//   slidesPerView: 2.5,
+//   freeMode: true,
+//   watchSlidesProgress: true,
+// });
+// const swiper2 = new Swiper(".mini-slider", {
+//   loop: true,
+//   spaceBetween: 10,
+//   thumbs: {
+//     swiper: swiper1,
+//   },
+// });
