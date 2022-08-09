a8c5cd8603ec07e394a59bf873388cee55f122b1
quarentyne <peresada1997@gmail.com>
2022-08-09 13:45:40 +0300

    project slider done

--- Ceramica/public/js/swiper.js
+++ Ceramica/public/js/swiper.js
@@ -14,26 +14,15 @@ const uderheaderSwiper = new Swiper('.underheader-slider', {
   },
 });
 
-// const projectsSwiper = new Swiper('.full-slider', {
-//   // slidesPerView: 0.8,
-//   loop: true,
-//   navigation: {
-//     nextEl: '.projects__next',
-//     prevEl: '.projects__prev',
-//   },
-// });
+const project = new Swiper(".project-carousel", {
+  spaceBetween: 40,
+  slidesPerView: 3,
+  watchSlidesProgress: true,
+  loop: true,
+});
+const projectElements = new Swiper(".project-slider", {
+  thumbs: {
+    swiper: project,
+  },
+});
 
-// const swiper1 = new Swiper(".mini-carousel", {
-//   loop: true,
-//   spaceBetween: 10,
-//   slidesPerView: 2.5,
-//   freeMode: true,
-//   watchSlidesProgress: true,
-// });
-// const swiper2 = new Swiper(".mini-slider", {
-//   loop: true,
-//   spaceBetween: 10,
-//   thumbs: {
-//     swiper: swiper1,
-//   },
-// });
