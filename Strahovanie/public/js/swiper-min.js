d5bf856453943c6fadc7eb89da1c73a7d53211e8
quarentyne <peresada1997@gmail.com>
2022-08-22 21:00:14 +0300

    arrange

---
+++ Strahovanie/public/js/swiper-min.js
@@ -0,0 +1,1 @@
+import Swiper from"https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";const arrange=new Swiper(".arrange__swiper",{slidesPerView:1,spaceBetween:500,pagination:{el:".swiper-pagination",type:"progressbar"}});
