cbf4e830e3dff169c45a6c60d4af511bfd57b631
quarentyne <peresada1997@gmail.com>
2022-08-23 19:02:32 +0300

    confince without slider

---
+++ Strahovanie/public/js/confidence-min.js
@@ -0,0 +1,1 @@
+import Swiper from"https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";const confidence=new Swiper(".confirence__swiper",{slidesPerView:1,spaceBetween:30,navigation:!1,navigation:{nextEl:".confirence__next",prevEl:".confirence__prev"}});
