8820177139f5df669ba53463a32d54d713a0146a
quarentyne <peresada1997@gmail.com>
2022-08-24 18:14:24 +0300

    confidence

--- Strahovanie/src/js/confidence.js
+++ Strahovanie/src/js/confidence.js
@@ -1,9 +1,8 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
 const confidence = new Swiper('.confirence__swiper', {
-  slidesPerView: 1,
+  slidesPerView: 3,
   spaceBetween: 30,
-  navigation: false,
   navigation: {
     nextEl: '.confirence__next',
     prevEl: '.confirence__prev',
