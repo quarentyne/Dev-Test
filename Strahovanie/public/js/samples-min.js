3a78e6b0e6aa43e8ce91a319d5b75b216b42d2c3
quarentyne <peresada1997@gmail.com>
2022-08-31 15:32:28 +0300

    samples

---
+++ Strahovanie/public/js/samples-min.js
@@ -0,0 +1,1 @@
+import Swiper from"https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";const confidence=new Swiper(".samples__slider",{slidesPerView:4,spaceBetween:30,navigation:{nextEl:".samples__next",prevEl:".samples__prev"}});
