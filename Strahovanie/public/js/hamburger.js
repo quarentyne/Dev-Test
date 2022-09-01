33cf52c8aa8f79c318d5ee8745b68d3530a5b3d7
quarentyne <peresada1997@gmail.com>
2022-09-01 15:39:24 +0300

    greencard mobile

--- Strahovanie/public/js/hamburger.js
+++ Strahovanie/public/js/hamburger.js
@@ -5,7 +5,9 @@ const openMenuButton = document.querySelector('.menu__open');
 
 closeMenuButton.addEventListener('click', () => {
   menu.classList.remove('nav_show');
+  document.querySelector('body').style.overflowY = "";
 });
 openMenuButton.addEventListener('click', () => {
   menu.classList.add('nav_show');
+  document.querySelector('body').style.overflowY = "hidden";
 })
