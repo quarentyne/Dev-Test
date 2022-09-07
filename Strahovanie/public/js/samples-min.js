6ce5b1d7d7f1049d0cf6ee4e25967bf69977df8e
quarentyne <peresada1997@gmail.com>
2022-09-07 20:54:56 +0300

    tourism mobile

--- Strahovanie/public/js/samples-min.js
+++
@@ -1,1 +0,0 @@
-import Swiper from"https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";const samples=new Swiper(".samples__slider",{slidesPerView:4,spaceBetween:30,navigation:{nextEl:".samples__next",prevEl:".samples__prev"}});
